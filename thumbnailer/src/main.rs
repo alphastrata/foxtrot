d916d44e6d860e83b22e9ea091fc9080b5ab5b3b
Jer <alphastrata@gmail.com>
2025-10-04 19:19:43 +1000

    update: use wgpu as the default for the thumbnailer

--- thumbnailer/src/main.rs
+++ thumbnailer/src/main.rs
@@ -38,7 +38,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let loader = std::thread::spawn(move || {
         let foxtrot_pipeline_start = std::time::Instant::now();
         use step::step_file::StepFile;
-        use triangulate::triangulate::triangulate4 as triangulate;
+        use triangulate::triangulate::wgpu_triangulate as triangulate;
 
         let data = std::fs::read(&input).expect("Could not open file");
         let flat = StepFile::strip_flatten(&data);
